bf425fc6e83d370990258d66d2c0b44e555d4a55
VinceZCL <64774232+VinceZCL@users.noreply.github.com>
2025-06-13 06:41:27 +0800

    WezTerm config on Native Windows

---
+++ wezterm.lua
@@ -0,0 +1,93 @@
+local wezterm = require("wezterm")
+
+return {
+	-- Use Git Bash as the default shell
+	default_prog = { "C:\\Program Files\\Git\\bin\\bash.exe", "--login", "-i" },
+
+	-- Font configuration for Windows
+	font = wezterm.font_with_fallback({
+		"JetBrainsMono Nerd Font",
+		"Cascadia Mono",
+		"Fira Code",
+		"Consolas",
+	}),
+	font_size = 11.5,
+
+	-- Set the color scheme
+	color_scheme = "Catppuccin Mocha",
+
+	-- Appearance
+	window_background_opacity = 0.90,
+	window_decorations = "RESIZE",
+	enable_tab_bar = true,
+	hide_tab_bar_if_only_one_tab = true,
+
+	initial_cols = 120,
+	initial_rows = 30,
+
+	-- Terminal behavior
+	enable_wayland = false,
+	term = "xterm-256color",
+
+	-- Keybindings (optional)
+	keys = {
+		{
+			key = "t",
+			mods = "CTRL|SHIFT",
+			action = wezterm.action.SpawnTab("CurrentPaneDomain"),
+		},
+		{
+			key = "w",
+			mods = "CTRL|SHIFT",
+			action = wezterm.action.CloseCurrentTab({ confirm = true }),
+		},
+		-- Split vertically (right side) with Ctrl+Shift+S
+		{
+			key = "s",
+			mods = "CTRL|SHIFT",
+			action = wezterm.action.SplitVertical({
+				domain = "CurrentPaneDomain",
+				args = { "C:\\Program Files\\Git\\bin\\bash.exe", "--login", "-i" },
+			}),
+		},
+
+		-- Split horizontally (below) with Ctrl+Shift+D
+		{
+			key = "d",
+			mods = "CTRL|SHIFT",
+			action = wezterm.action.SplitHorizontal({
+				domain = "CurrentPaneDomain",
+				args = { "C:\\Program Files\\Git\\bin\\bash.exe", "--login", "-i" },
+			}),
+		},
+
+		{
+			key = "a",
+			mods = "CTRL",
+			action = wezterm.action.ActivatePaneDirection("Left"),
+		},
+		{
+			key = "d",
+			mods = "CTRL",
+			action = wezterm.action.ActivatePaneDirection("Right"),
+		},
+		{
+			key = "w",
+			mods = "CTRL",
+			action = wezterm.action.ActivatePaneDirection("Up"),
+		},
+		{
+			key = "s",
+			mods = "CTRL",
+			action = wezterm.action.ActivatePaneDirection("Down"),
+		},
+	},
+	mouse_bindings = {
+		{
+			event = { Down = { streak = 1, button = "Left" } },
+			mods = "ALT",
+			action = wezterm.action.StartWindowDrag,
+		},
+	},
+}
+
